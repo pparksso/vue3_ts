b511d37725ca0e58b8e618dfdea86d8ab6e17ee1
pparksso <thgml5143@hanmail.net>
2022-12-12 23:27:29 +0900

    타입스크립트 연습

---
+++ src/components/TimeLine.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+const periods = ['Today', 'This Week', 'This Month']
+let selectedPeriod = ref('Today');
+function selectPeriod(period: string) {
+  console.log(period)
+  selectedPeriod.value = period
+}
+</script>
+<template>
+  <nav class="is-primary panel">
+    {{selectedPeriod}}
+    <span class="panel-tabs">
+      <a v-for="period of periods" :key="period" @click="selectPeriod(period)">{{period}}</a>
+    </span>
+  </nav>
+
+</template>

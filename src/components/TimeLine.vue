c7516ca6b1fd82bcbcf2b2bd67fbaa94875480a0
Park Sohee <pssh5124@gmail.com>
2022-12-13 10:09:52 +0900

    chore: luxon 설치

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
 import {ref} from 'vue'
-const periods = ['Today', 'This Week', 'This Month']
-let selectedPeriod = ref('Today');
-function selectPeriod(period: string) {
-  console.log(period)
+import {Post, today, thisMonth, thisWeek} from '../posts'
+import { DateTime } from 'luxon'
+
+const periods = ['Today', 'This Week', 'This Month']as const
+type Period = typeof periods[number]
+
+const selectedPeriod = ref<Period>('Today');
+function selectPeriod(period: Period) {
   selectedPeriod.value = period
 }
+
+const posts = [today,thisWeek, thisMonth].map(post=> {
+  return {
+    ...post,
+    created: DateTime.fromISO(post.created)
+  }
+})
 </script>
 <template>
   <nav class="is-primary panel">
     {{selectedPeriod}}
     <span class="panel-tabs">
-      <a v-for="period of periods" :key="period" @click="selectPeriod(period)">{{period}}</a>
+      <a v-for="period of periods" :key="period" @click="selectPeriod(period)" :class="{'is-active' : period === selectedPeriod}">{{period}}</a>
     </span>
+    <a v-for="post of posts" :key="post.id" class="panel-block">
+    <a>{{post.title}}</a>
+    <div class="">{{post.created.toFormat('d MMM')}}</div>
+    </a>
   </nav>
 
 </template>

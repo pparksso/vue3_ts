bbb27fd83e0a5c3b8cdc60816bf3db09020ef6e2
Park Sohee <pssh5124@gmail.com>
2022-12-15 09:23:18 +0900

    직전 코드

--- src/components/TimeLine.vue
+++ src/components/TimeLine.vue
@@ -26,7 +26,7 @@ const posts = [today,thisWeek, thisMonth].map(post=> {
     </span>
     <a v-for="post of posts" :key="post.id" class="panel-block">
     <a>{{post.title}}</a>
-    <div class="">{{post.created.toFormat('d MMM')}}</div>
+    <div class="">{{post.created.toFormat('d.MMM')}}</div>
     </a>
   </nav>
 

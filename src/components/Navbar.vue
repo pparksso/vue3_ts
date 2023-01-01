601e4f3b9e8e6183f5db2897e9a97d83ec75029d
pparksso <thgml5143@hanmail.net>
2023-01-02 06:55:55 +0900

    57 end

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
 import { useModal } from "../composables/modal";
 import { useUser } from "../store/users";
+import { useRouter } from "vue-router";
 
 const userStore = useUser();
 const modal = useModal();
+const router = useRouter();
+
+async function logout() {
+  userStore.logout();
+  router.push({ path: "/" });
+}
 </script>
 <template>
   <div class="navbar">
     <div class="navbar-end">
       <div class="buttons" v-if="userStore.currentUserId">
         <router-link to="/posts/new" class="button"> New Post </router-link>
-        <button class="button" @click="userStore.logout()">Log Out</button>
+        <button class="button" @click="logout">Log Out</button>
       </div>
       <div class="buttons" v-else>
         <button class="button" @click="modal.showModal('signUp')">Sign up</button>
--- src/router.ts
+++ src/router.ts
@@ -1,15 +1,26 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import Home from './views/Home.vue'
-import NewPost from './views/NewPost.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./views/Home.vue";
+import NewPost from "./views/NewPost.vue";
+import { useUser } from "./store/users";
 
 export const router = createRouter({
-  history : createWebHistory(),
-  routes:[
+  history: createWebHistory(),
+  routes: [
     {
-      path:'/',
-      component:Home
+      path: "/",
+      component: Home,
     },
-    {path:'/posts/new',
-  component:NewPost}
-  ]
-})
+    {
+      path: "/posts/new",
+      component: NewPost,
+      beforeEnter: () => {
+        const userStore = useUser();
+        if (!userStore.currentUserId) {
+          return {
+            path: "/",
+          };
+        }
+      },
+    },
+  ],
+});

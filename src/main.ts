8c70427faaf0756df4df60f36c60b0f0ca1acbcf
pparksso <thgml5143@hanmail.net>
2023-01-03 06:51:09 +0900

    not build

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import {router} from './router'
-import {createPinia} from 'pinia'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { router } from "./router";
+import { createPinia } from "pinia";
+import { useUser } from "./store/users";
+import { usePosts } from "./store/posts";
 
-createApp(App).use(createPinia()).use(router).mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+
+const userStore = useUser();
+const postsStore = usePosts();
+
+Promise.all([userStore.authenticate(), postsStore.fetchPosts()]).then(() => {
+  app.use(router);
+  app.mount("#app");
+});
